6e0e9d8f6aaf2dd2ed1daf06bc1bac0665e54bd5
Dorottya Katona <moodytea@yahoo.com>
2018-01-10 14:29:28 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ QuizApp/QuizApp/Entities/QuestionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QuizApp.Models;
+
+namespace QuizApp.Entities
+{
+    public class QuestionContext : DbContext
+    {
+        public QuestionContext(DbContextOptions<QuestionContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Question> Questions { get; set; }
+    }
+}
