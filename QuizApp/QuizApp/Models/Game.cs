99c9bbfa71e7b19edd853e3489dc09bea82e0667
Görföl Botond <botond.gorfol@gmail.com>
2018-01-10 10:39:06 +0100

    create game and question model

---
+++ QuizApp/QuizApp/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.Models
+{
+    public class Game
+    {
+        public string PlayerName { get; set; } = "Foxey";
+        public int LevelAchieved { get; set; }
+        public bool PhoneHelpIsAvailable { get; set; } = true;
+        public bool AudienceHelpIsAvailable { get; set; } = true;
+        public bool HalferHelpIsAvailable { get; set; } = true;
+        public Question ActualQuestion { get; set; }
+        public List<Question> AvailableQuestions { get; set; }
+        public bool IsWon { get; set; }
+    }
+}
---
+++ QuizApp/QuizApp/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The right answer is always the first one in the array (index 0).
+        /// </summary>
+        public string[] Answers { get; set; }
+    }
+}
